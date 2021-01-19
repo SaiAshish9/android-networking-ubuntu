6e0eab1d3bf090e84ba5cc6f8c2e4b6344146367
saiashish9 <saiashish3760@gmail.com>
2021-01-19 22:40:46 +0530

    splash screen updated

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,61 +1,17 @@
 package com.example.networking;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
-
-
-    private final static String URL = "https://jsonplaceholder.typicode.com/todos";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
+        setContentView(R.layout.activity_main2);
 
-                        for(int i=0;i<response.length();i++){
-                            try{
-                                JSONObject x = response.getJSONObject(i);
-                                Log.d("Items:",x.getString("id"));
-                            }catch (JSONException e){
-                                e.printStackTrace();
-                            }
-                        }
-
-//                        String Request  hi
-//                        JSONObjectRequest
-
-//                        Log.d("Response", response.toString());
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("Error", error.getMessage());
-            }
-        }
-        );
-        queue.add(arrayRequest);
     }
 
 }
---
+++ app/src/main/java/com/example/networking/Model/Movie.java
@@ -0,0 +1,14 @@
+package com.example.networking.Model;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+    private static final long id = 1L;
+
+    private String title;
+    private String director;
+
+
+
+}
